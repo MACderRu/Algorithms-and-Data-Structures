6813ce82339e6f5a8af23788cb7c2756d87f68dc
vadim_titov <titov.vn@phystech.edu>
2020-03-17 14:55:08 +0300

    tail recursion elimination and recursion logN depth guaranteed

--- qsort.cpp
+++ qsort.cpp
@@ -7,13 +7,22 @@
 std::pair<int*, int> read_array(std::string input_s) {
     int length = std::count(input_s.begin(), input_s.end(), ' ') + 1;
     int* arr = new int[length];
+    int flag[length];
+    for (int i = 0; i < length; i++) {
+        flag[i] = 1;
+    }
+
     int i = 0, j = 0;
     for (int i = 0; i < length; i++) {
         arr[i] = 0;
     }
     while (i < input_s.size()) {
         if (input_s[i] != ' ') {
-            arr[j] = arr[j] * 10 + (input_s[i] - 48);
+            if (input_s[i] == '-') {
+                flag[j] = -1;
+            } else {
+                arr[j] = arr[j] * 10 + flag[j] * (input_s[i] - 48);
+            }
         } else {
             j++;
         }
@@ -26,6 +35,8 @@ std::pair<int*, int> read_array(std::string input_s) {
 void qsort(int* arr, int l, int r) {
 
     if (l < r) {
+
+
         int pivot_idx = l, j = l;
 
         int pivot = arr[pivot_idx];
@@ -47,6 +58,57 @@ void qsort(int* arr, int l, int r) {
 
 }
 
+void qsort_tail_eliminated(int* arr, int l, int r) {
+
+    while (l < r) {
+        int pivot_idx = l, j = l;
+
+        int pivot = arr[pivot_idx];
+        std::swap(arr[pivot_idx], arr[r]);
+
+        for (int i = l; i < r; i++) {
+            if (arr[i] <= pivot) {
+                std::swap(arr[i], arr[j]);
+                j++;
+            }
+        }
+
+        std::swap(arr[r], arr[j]);
+
+        qsort_tail_eliminated(arr, l, j - 1);
+        l = j + 1;
+    }
+}
+
+
+
+void qsort_faster(int* arr, int l, int r) {
+
+    while (l < r) {
+        int pivot_idx = l, j = l;
+
+        int pivot = arr[pivot_idx];
+        std::swap(arr[pivot_idx], arr[r]);
+
+        for (int i = l; i < r; i++) {
+            if (arr[i] <= pivot) {
+                std::swap(arr[i], arr[j]);
+                j++;
+            }
+        }
+        std::swap(arr[r], arr[j]);
+
+        if (j >= (l + r) / 2) {
+            qsort_faster(arr, j + 1, r);
+            r = j - 1;
+        } else {
+            qsort_faster(arr, l, j - 1);
+            l = j + 1;
+        }
+
+    }
+
+}
 
 int main() {
 
@@ -57,7 +119,7 @@ int main() {
     int* arr;
     std::tie(arr, N) = read_array(input_s);
 
-    qsort(arr, 0, N - 1);
+    qsort_faster(arr, 0, N - 1);
 
     for (int i = 0; i < N; i++) {
         std::cout << arr[i] << ' ';
